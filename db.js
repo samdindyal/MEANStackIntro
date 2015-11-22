3a957bb42d2295b8237d4d7c76c99ff81efeeb00
Samuel Dindyal <sam.dindyal@icloud.com>
2015-11-22 15:20:44 -0500

    Database connected added

--- db.js
+++ db.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://cps530usr:lemotdepasse@ds057204.mongolab.com:57204/cps530exampleapp', function() {
-  console.log('mongodb connected');
-});
+if (process.env.MONGOLAB_URI)
+  mongoose.connect(process.env.MONGOLAB_URI, function() {
+    console.log('mongodb connected');
+  });
 
 module.exports = mongoose;
