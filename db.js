4d90cebf72a9e01eacb47ca5cfebff627010fb71
Sam Dindyal <samdindyal@users.noreply.github.com>
2015-11-22 15:05:32 -0500

    Delete db.js

--- db.js
+++
@@ -1,6 +0,0 @@
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://cps530usr:lemotdepasse@ds057204.mongolab.com:57204/cps530exampleapp', function() {
-  console.log('mongodb connected');
-});
-
-module.exports = mongoose;
