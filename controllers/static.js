dbf7e8002efc0e023fd36c12d29079655764dd1e
Samuel Dindyal <sam.dindyal@icloud.com>
2015-11-22 13:23:52 -0500

    Added templates for all pages

--- controllers/static.js
+++ controllers/static.js
@@ -9,6 +9,30 @@ router.get('/introduction', function(req, res) {
   res.sendfile('layouts/introduction.html');
 });
 
+router.get('/example', function(req, res) {
+  res.sendfile('layouts/example-app.html');
+});
+
+router.get('/setup', function(req, res) {
+  res.sendfile('layouts/setup.html');
+});
+
+router.get('/simplewebapp', function(req, res) {
+  res.sendfile('layouts/simplewebapp.html');
+});
+
+router.get('/deploy', function(req, res) {
+  res.sendfile('layouts/deploy.html');
+});
+
+router.get('/experience', function(req, res) {
+  res.sendfile('layouts/experience.html');
+});
+
+router.get('/contributions', function(req, res) {
+  res.sendfile('layouts/contributions.html');
+});
+
 router.use(express.static(__dirname + '/../assets'));
 
 module.exports = router;
