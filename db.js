1573401a81ea0a96db69a5520184b0cc37e14e4f
Samuel Dindyal <sam.dindyal@icloud.com>
2015-11-22 15:02:34 -0500

    Database connection added

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-mongoose.connect('', function() {
+mongoose.connect('mongodb://cps530usr:lemotdepasse@ds057204.mongolab.com:57204/cps530exampleapp', function() {
   console.log('mongodb connected');
 });
 
