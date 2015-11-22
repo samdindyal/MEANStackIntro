bdf9e8f3f28434111727e830768dfdc8d48cb294
Samuel Dindyal <sam.dindyal@icloud.com>
2015-11-22 12:03:02 -0500

    Started intro page

--- controllers/static.js
+++ controllers/static.js
@@ -5,6 +5,10 @@ router.get('/', function(req, res) {
   res.sendfile('layouts/splashpage.html');
 });
 
+router.get('/introduction', function(req, res) {
+  res.sendfile('layouts/introduction.html');
+});
+
 router.use(express.static(__dirname + '/../assets'));
 
 module.exports = router;
