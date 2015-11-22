b1ba81e2c71ca74b470ffe08fc4f2c4d6be85016
Samuel Dindyal <sam.dindyal@icloud.com>
2015-11-22 18:24:54 -0500

    Maximum inputs added

--- db.js
+++ db.js
@@ -1,7 +1,8 @@
 var mongoose = require('mongoose');
+
 if (process.env.MONGOLAB_URI)
   mongoose.connect(process.env.MONGOLAB_URI, function() {
     console.log('mongodb connected');
-  });
+});
 
 module.exports = mongoose;
