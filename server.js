ae4c517fb8dd7687574d96cb4ce1a4fe593ddeec
Samuel Dindyal <sam.dindyal@icloud.com>
2015-11-22 00:07:49 -0500

    Port error fix

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
+var port = process.env.PORT || 3000;
+
 var app = express();
 app.use(bodyParser.json());
 
 app.use(require('./controllers/static'));
 
-app.listen(3000, function() {
-  console.log('Server listening on', 3000);
+app.listen(port, function() {
+  console.log('Server listening on', port);
 });
