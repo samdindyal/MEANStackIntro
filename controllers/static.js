8448dd20e9887fe9ccac9a4241b80b1d030d9850
Samuel Dindyal <sam.dindyal@icloud.com>
2015-11-21 23:37:19 -0500

    First commit

---
+++ controllers/static.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res) {
+  res.sendfile('layouts/splashpage.html');
+});
+
+router.use(express.static(__dirname + '/../assets'));
+
+module.exports = router;
