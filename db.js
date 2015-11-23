74fc4d94a045952e3b4f1506711c9baf31059a9a
Samuel Dindyal <sam.dindyal@icloud.com>
2015-11-23 02:23:15 -0500

    fixed untracked files

--- db.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-
-if (process.env.MONGOLAB_URI)
-  mongoose.connect(process.env.MONGOLAB_URI, function() {
-    console.log('mongodb connected');
-});
-
-module.exports = mongoose;
